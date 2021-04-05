cee4874f362be08448b81dc1d4e307b23e2586a8
Deqing Sun <iamseer@gmail.com>
2021-04-05 00:46:59 -0400

    add comment

--- examples/attiny85_rc_calibration/attiny85_rc_calibration.ino
+++ examples/attiny85_rc_calibration/attiny85_rc_calibration.ino
@@ -1,3 +1,35 @@
+/*
+   This example will use the calibration method mentioned in
+   AVR053: Internal RC Oscillator Calibration for. tinyAVR and megaAVR Devices
+   to calibrate the internal RC oscillator for improved accuracy.
+
+   If your target AVR chip does not use with an external crystal or ceramic oscillator,
+   nor having an external timing refernce. You may use this example to improved accuracy.
+
+   For example, the Attiny85 is factory calibrated with a 10% accuracy rating.
+   In my tests, they are often at 3% accuracy. After the calibration, 1% accuracy can be achieved.
+   Although long-term stability is not tested yet, calibration will provide much more accurate timing.
+   Especially you do the calibration with the same voltage and temperature to the working environment.
+
+   The calibration of AVR is controlled by OSCCAL register. By default it is loaded with a factory calibrated value.
+   And we can use another value to replace it for better accuracy.
+
+   This example will calibrate the Attiny85 at 8Mhz frequency.
+
+   First the Attiny85 is programmed with correct fuse settings for 8Mhz clock.
+
+   Then the rc_calib_tiny85.hex firmware will be flashed. The rc_calib_tiny85.hex is compiled with AVR053 examples,
+   with "matching device", "interface" and "TARGET_FREQ" modified. AVR Studio 4 was used for compilation.
+
+   After that the calibration begins, the reset was pulled down, and a 32787Hz frequency was generated on MOSI.
+   Then the reset was released. When the firmware calibrated itself,
+   it will put the calibrated value in the 0 address of EEPROM and notify the host with 4 pulses on the MISO line.
+
+   The next step is to read back the calibrated OSCCAL value from EEPROM. And flash a final blinkLED.ino.gemma.hex firmware.
+   This tool will write a copy of the calibrated OSCCAL value to flash address 0x3FF, just to prevent EEPROM corruption.
+   And there should be an accurate 1Khz signal on the PB1 port that you can measure with an instrument.
+*/
+
 #include "Adafruit_AVRProg.h"
 
 Adafruit_AVRProg avrprog = Adafruit_AVRProg();
