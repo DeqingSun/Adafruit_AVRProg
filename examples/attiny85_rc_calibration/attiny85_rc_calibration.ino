719cb3e6d0654c631a07cae357b9d3a444b5dec3
Deqing Sun <iamseer@gmail.com>
2021-04-05 00:49:39 -0400

    format

--- examples/attiny85_rc_calibration/attiny85_rc_calibration.ino
+++ examples/attiny85_rc_calibration/attiny85_rc_calibration.ino
@@ -3,31 +3,39 @@
    AVR053: Internal RC Oscillator Calibration for. tinyAVR and megaAVR Devices
    to calibrate the internal RC oscillator for improved accuracy.
 
-   If your target AVR chip does not use with an external crystal or ceramic oscillator,
-   nor having an external timing refernce. You may use this example to improved accuracy.
+   If your target AVR chip does not use with an external crystal or ceramic
+   oscillator, nor having an external timing refernce. You may use this example
+   to improved accuracy.
 
    For example, the Attiny85 is factory calibrated with a 10% accuracy rating.
-   In my tests, they are often at 3% accuracy. After the calibration, 1% accuracy can be achieved.
-   Although long-term stability is not tested yet, calibration will provide much more accurate timing.
-   Especially you do the calibration with the same voltage and temperature to the working environment.
+   In my tests, they are often at 3% accuracy. After the calibration, 1%
+   accuracy can be achieved. Although long-term stability is not tested yet,
+   calibration will provide much more accurate timing. Especially you do the
+   calibration with the same voltage and temperature to the working environment.
 
-   The calibration of AVR is controlled by OSCCAL register. By default it is loaded with a factory calibrated value.
-   And we can use another value to replace it for better accuracy.
+   The calibration of AVR is controlled by OSCCAL register. By default it is
+   loaded with a factory calibrated value. And we can use another value to
+   replace it for better accuracy.
 
    This example will calibrate the Attiny85 at 8Mhz frequency.
 
    First the Attiny85 is programmed with correct fuse settings for 8Mhz clock.
 
-   Then the rc_calib_tiny85.hex firmware will be flashed. The rc_calib_tiny85.hex is compiled with AVR053 examples,
-   with "matching device", "interface" and "TARGET_FREQ" modified. AVR Studio 4 was used for compilation.
+   Then the rc_calib_tiny85.hex firmware will be flashed. The
+   rc_calib_tiny85.hex is compiled with AVR053 examples, with "matching device",
+   "interface" and "TARGET_FREQ" modified. AVR Studio 4 was used for
+   compilation.
 
-   After that the calibration begins, the reset was pulled down, and a 32787Hz frequency was generated on MOSI.
-   Then the reset was released. When the firmware calibrated itself,
-   it will put the calibrated value in the 0 address of EEPROM and notify the host with 4 pulses on the MISO line.
+   After that the calibration begins, the reset was pulled down, and a 32787Hz
+   frequency was generated on MOSI. Then the reset was released. When the
+   firmware calibrated itself, it will put the calibrated value in the 0 address
+   of EEPROM and notify the host with 4 pulses on the MISO line.
 
-   The next step is to read back the calibrated OSCCAL value from EEPROM. And flash a final blinkLED.ino.gemma.hex firmware.
-   This tool will write a copy of the calibrated OSCCAL value to flash address 0x3FF, just to prevent EEPROM corruption.
-   And there should be an accurate 1Khz signal on the PB1 port that you can measure with an instrument.
+   The next step is to read back the calibrated OSCCAL value from EEPROM. And
+   flash a final blinkLED.ino.gemma.hex firmware. This tool will write a copy of
+   the calibrated OSCCAL value to flash address 0x3FF, just to prevent EEPROM
+   corruption. And there should be an accurate 1Khz signal on the PB1 port that
+   you can measure with an instrument.
 */
 
 #include "Adafruit_AVRProg.h"
@@ -107,7 +115,7 @@ void loop(void) {
   Serial.println(F("Done!"));
 
   if (!avrprog.programFuses(
-        targetimage->image_progfuses)) { // get fuses ready to program
+          targetimage->image_progfuses)) { // get fuses ready to program
     avrprog.error(F("Programming Fuses fail"));
   }
 
@@ -170,10 +178,10 @@ void loop(void) {
   }
   Serial.println(F("\nFinal Flash verified correctly!"));
 
-  Serial.println(F("\nWrite calibrated OSCCAL to flash for more persistent storage"));
-  if (!avrprog.writeByteToFlash(0x3FF,
-                                pgm_read_byte(&finalimage->image_pagesize),
-                                osscal_value)) {
+  Serial.println(
+      F("\nWrite calibrated OSCCAL to flash for more persistent storage"));
+  if (!avrprog.writeByteToFlash(
+          0x3FF, pgm_read_byte(&finalimage->image_pagesize), osscal_value)) {
     avrprog.error(F("Failed to write byte in flash"));
   }
 
--- examples/attiny85_rc_calibration/images.cpp
+++ examples/attiny85_rc_calibration/images.cpp
@@ -1,32 +1,32 @@
 #include "Adafruit_AVRProg.h"
 
 const image_t PROGMEM image_calibration = {
-  // Sketch name, only used for serial printing
-  {"rc_calib_tiny85.hex"},
-  // Chip name, only used for serial printing
-  {"attiny85"},
-  // Signature bytes for 328P
-  0x930B,
-  // Programming fuses, written before writing to flash. Fuses set to
-  // zero are untouched.
-  {0x03, 0xE2, 0xDD, 0xFF}, // {lock, low, high, extended}
-  // Normal fuses, written after writing to flash (but before
-  // verifying). Fuses set to zero are untouched.
-  {0x0, 0x0, 0xD5, 0x0}, // {lock, low, high, extended}
-  // Fuse verify mask. Any bits set to zero in these values are
-  // ignored while verifying the fuses after writing them. All (and
-  // only) bits that are unused for this atmega chip should be zero
-  // here.
-  {0x03, 0xFF, 0xFF, 0xFF}, // {lock, low, high, extended}
-  // size of chip flash in bytes
-  8192,
-  // size in bytes of flash page
-  64,
-  // The actual image to flash. This can be copy-pasted as-is from a
-  // .hex file. If you do, replace all lines below starting with a
-  // colon, but make sure to keep the start and end markers {R"( and
-  // )"} in place.
-  { R"(
+    // Sketch name, only used for serial printing
+    {"rc_calib_tiny85.hex"},
+    // Chip name, only used for serial printing
+    {"attiny85"},
+    // Signature bytes for 328P
+    0x930B,
+    // Programming fuses, written before writing to flash. Fuses set to
+    // zero are untouched.
+    {0x03, 0xE2, 0xDD, 0xFF}, // {lock, low, high, extended}
+    // Normal fuses, written after writing to flash (but before
+    // verifying). Fuses set to zero are untouched.
+    {0x0, 0x0, 0xD5, 0x0}, // {lock, low, high, extended}
+    // Fuse verify mask. Any bits set to zero in these values are
+    // ignored while verifying the fuses after writing them. All (and
+    // only) bits that are unused for this atmega chip should be zero
+    // here.
+    {0x03, 0xFF, 0xFF, 0xFF}, // {lock, low, high, extended}
+    // size of chip flash in bytes
+    8192,
+    // size in bytes of flash page
+    64,
+    // The actual image to flash. This can be copy-pasted as-is from a
+    // .hex file. If you do, replace all lines below starting with a
+    // colon, but make sure to keep the start and end markers {R"( and
+    // )"} in place.
+    {R"(
 :1000000009C0FFFFFFFFFFFFFFFFFFFFFFFFFFFF35
 :10001000FFFFFFFF002401E0102E0FE50DBF02E0FF
 :100020000EBFC09AC19AB99A1CD000E00FBB00E085
@@ -52,37 +52,35 @@ const image_t PROGMEM image_calibration = {
 :1001600011EC25E21215230524F440E0089541E046
 :0601700008954FEF089511
 :00000001FF
-    )"
-  }
-};
+    )"}};
 
 const image_t PROGMEM image_blinkLED = {
-  // Sketch name, only used for serial printing
-  {"blinkLED.hex"},
-  // Chip name, only used for serial printing
-  {"attiny85"},
-  // Signature bytes for 328P
-  0x930B,
-  // Programming fuses, written before writing to flash. Fuses set to
-  // zero are untouched.
-  {0x03, 0xE2, 0xDD, 0xFF}, // {lock, low, high, extended}
-  // Normal fuses, written after writing to flash (but before
-  // verifying). Fuses set to zero are untouched.
-  {0x0, 0x0, 0xD5, 0x0}, // {lock, low, high, extended}
-  // Fuse verify mask. Any bits set to zero in these values are
-  // ignored while verifying the fuses after writing them. All (and
-  // only) bits that are unused for this atmega chip should be zero
-  // here.
-  {0x03, 0xFF, 0xFF, 0xFF}, // {lock, low, high, extended}
-  // size of chip flash in bytes
-  8192,
-  // size in bytes of flash page
-  64,
-  // The actual image to flash. This can be copy-pasted as-is from a
-  // .hex file. If you do, replace all lines below starting with a
-  // colon, but make sure to keep the start and end markers {R"( and
-  // )"} in place.
-  { R"(
+    // Sketch name, only used for serial printing
+    {"blinkLED.hex"},
+    // Chip name, only used for serial printing
+    {"attiny85"},
+    // Signature bytes for 328P
+    0x930B,
+    // Programming fuses, written before writing to flash. Fuses set to
+    // zero are untouched.
+    {0x03, 0xE2, 0xDD, 0xFF}, // {lock, low, high, extended}
+    // Normal fuses, written after writing to flash (but before
+    // verifying). Fuses set to zero are untouched.
+    {0x0, 0x0, 0xD5, 0x0}, // {lock, low, high, extended}
+    // Fuse verify mask. Any bits set to zero in these values are
+    // ignored while verifying the fuses after writing them. All (and
+    // only) bits that are unused for this atmega chip should be zero
+    // here.
+    {0x03, 0xFF, 0xFF, 0xFF}, // {lock, low, high, extended}
+    // size of chip flash in bytes
+    8192,
+    // size in bytes of flash page
+    64,
+    // The actual image to flash. This can be copy-pasted as-is from a
+    // .hex file. If you do, replace all lines below starting with a
+    // colon, but make sure to keep the start and end markers {R"( and
+    // )"} in place.
+    {R"(
 :100000000EC015C014C013C012C011C010C00FC064
 :100010000EC00DC00CC00BC00AC009C008C011241E
 :100020001FBECFE5D2E0DEBFCDBF02D01CC0E8CFFF
@@ -91,16 +89,14 @@ const image_t PROGMEM image_blinkLED = {
 :10005000815081BFF9CFB99A82E18ABD83E083BF25
 :0A0060008CE789BDFFCFF894FFCFB5
 :00000001FF
-    )"
-  }
-};
+    )"}};
 
 /*
    Table of defined images
 */
 const image_t *images[] = {
-  &image_calibration,
-  &image_blinkLED,
+    &image_calibration,
+    &image_blinkLED,
 };
 
 uint8_t NUMIMAGES = sizeof(images) / sizeof(images[0]);
